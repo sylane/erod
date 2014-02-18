4f4f696fb36b7840c9eaa2569ac7bc793d200748
Sebastien Merle <smerle@voalte.com>
2014-02-18 02:24:14 +0100

    Removes the need to build a custom comparison function every time.

--- apps/erod/src/erod_indices.erl
+++ apps/erod/src/erod_indices.erl
@@ -1,13 +1,16 @@
 -module(erod_indices).
 
 -export([new/0,
-         from_list/2,
-         from_ordset/1,
-         to_list/1,
+         from_indice/1,
+         from_ordered/1,
+         for_map/2,
+         keys/1,
+         values/2,
+         to_list/2,
          size/1,
-         index/3,
-         insert/3,
-         delete/3,
+         index/4, index/5,
+         insert/4, insert/5,
+         delete/4, delete/5,
          smallest/1,
          take_smallest/1,
          largest/1,
@@ -26,35 +29,67 @@ new() ->
     {?MODULE, nil}.
 
 
-from_list(Values, OrderFun) ->
-    from_ordset(lists:usort(OrderFun, Values)).
+from_indice(Keys) ->
+    {?MODULE, balance_list(Keys, length(Keys))}.
 
 
-from_ordset(Values) ->
-    {?MODULE, balance_list(Values, length(Values))}.
+from_ordered(Items) ->
+    Keys = [K || {K, _} <- Items],
+    {?MODULE, balance_list(Keys, length(Keys))}.
 
 
-to_list({?MODULE, Tree}) ->
+for_map(CompareFun, Map) ->
+    Fun = fun(Ka, Ka) -> true;
+             (Ka, Kb) -> CompareFun(erod_maps:value(Ka, Map),
+                                    erod_maps:value(Kb, Map))
+          end,
+    from_indice(lists:usort(Fun, erod_maps:keys(Map))).
+
+
+keys({?MODULE, Tree}) ->
     tree_to_list(Tree, []).
+
+
+values(Map, {?MODULE, Tree}) ->
+    [V || K <- tree_to_list(Tree, []),
+          begin V = erod_maps:value(K, Map), true end].
+
+
+to_list(Map, {?MODULE, Tree}) ->
+    [{K, V} || K <- tree_to_list(Tree, []),
+               begin V = erod_maps:value(K, Map), true end].
 
 
 size({?MODULE, Tree}) ->
     tree_size(Tree).
 
 
-index(Value, OrderFun, {?MODULE, Tree}) ->
-    tree_index(Value, OrderFun, Tree).
+index(Key, CompareFun, Map, Indice) ->
+    index(Key, erod_maps:value(Key, Map), CompareFun, Map, Indice).
 
 
-insert(Value, OrderFun, {?MODULE, Tree}) ->
+index(Key, Value, CompareFun, Map, {?MODULE, Tree}) ->
+    tree_index(Key, Value, CompareFun, Map, Tree).
+
+
+insert(Key, CompareFun, Map, Indice) ->
+    insert(Key, erod_maps:value(Key, Map), CompareFun, Map, Indice).
+
+
+insert(Key, Value, CompareFun, Map, {?MODULE, Tree}) ->
     Size = tree_size(Tree),
     WeightRef = ?pow(Size + 1),
-    {_, Idx, NewTree} = tree_insert(Value, OrderFun, Tree, WeightRef),
+    {_, Idx, NewTree} = tree_insert(Key, Value, CompareFun, Map,
+                                    Tree, WeightRef),
     {Idx, {?MODULE, NewTree}}.
 
 
-delete(Value, OrderFun, {?MODULE, Tree}) ->
-    {Idx, NewTree} = tree_delete(Value, OrderFun, Tree),
+delete(Key, CompareFun, Map, Indice) ->
+    delete(Key, erod_maps:value(Key, Map), CompareFun, Map, Indice).
+
+
+delete(Key, Value, CompareFun, Map, {?MODULE, Tree}) ->
+    {Idx, NewTree} = tree_delete(Key, Value, CompareFun, Map, Tree),
     {Idx, {?MODULE, NewTree}}.
 
 
@@ -85,40 +120,40 @@ balance({?MODULE, Tree}) ->
 tree_to_list(Tree) -> tree_to_list(Tree, []).
 
 
-tree_to_list({Value, _Size, _Weight, Smaller, Larger}, Acc) ->
-    tree_to_list(Smaller, [Value | tree_to_list(Larger, Acc)]);
+tree_to_list({Key, _Size, _Weight, Smaller, Larger}, Acc) ->
+    tree_to_list(Smaller, [Key | tree_to_list(Larger, Acc)]);
 
 tree_to_list(nil, Acc) -> Acc.
 
 
 tree_size(nil) -> 0;
 
-tree_size({_Value, Size, _Weight, _Smaller, _Larger}) -> Size.
+tree_size({_Key, Size, _Weight, _Smaller, _Larger}) -> Size.
 
 
-tree_index(RefVal, _OrderFun, {RefVal, _Size, _Weight, Smaller, _Larger}) ->
+tree_index(RefKey, _Val, _Comp, _Map, {RefKey, _S, _W, Smaller, _Larger}) ->
     tree_size(Smaller) + 1;
 
-tree_index(Value, OrderFun, {RefVal, _Size, _Weight, Smaller, Larger}) ->
-    case OrderFun(Value, RefVal) of
+tree_index(Key, Val, Comp, Map, {RefKey, _S, _W, Smaller, Larger}) ->
+    case Comp(Val, erod_maps:value(RefKey, Map)) of
         true ->
-            tree_index(Value, OrderFun, Smaller);
+            tree_index(Key, Val, Comp, Map, Smaller);
         false ->
-            tree_size(Smaller) + 1 + tree_index(Value, OrderFun, Larger)
+            tree_size(Smaller) + 1 + tree_index(Key, Val, Comp, Map, Larger)
     end.
 
 
-tree_insert(RefVal, _OrderFun, {RefVal, _Size, _Weight, _Sm, _Lg}, _WRef) ->
-    erlang:error({key_already_exists, RefVal});
+tree_insert(RefKey, _Val, _Comp, _Map, {RefKey, _S, _W, _Sm, _Lg}, _WRef) ->
+    erlang:error({key_already_exists, RefKey});
 
-tree_insert(Value, OrderFun, {RefVal, _S, _W, Smaller, Larger}, WRef) ->
-    case OrderFun(Value, RefVal) of
+tree_insert(Key, Val, Comp, Map, {RefKey, _S, _W, Smaller, Larger}, WRef) ->
+    case Comp(Val, erod_maps:value(RefKey, Map)) of
         true ->
-            case tree_insert(Value, OrderFun, Smaller, ?div2(WRef)) of
+            case tree_insert(Key, Val, Comp, Map, Smaller, ?div2(WRef)) of
                 {ok, Idx, NewSmaller} ->
-                    {ok, Idx, combinate(RefVal, NewSmaller, Larger)};
+                    {ok, Idx, combinate(RefKey, NewSmaller, Larger)};
                 {check, Idx, NewSmaller} ->
-                    NewTree = combinate(RefVal, NewSmaller, Larger),
+                    NewTree = combinate(RefKey, NewSmaller, Larger),
                     {_, Size, Weight, _, _} = NewTree,
                     NewWRef = ?pow(Size),
                     if Weight > NewWRef -> {ok, Idx, balance_tree(NewTree)};
@@ -126,13 +161,13 @@ tree_insert(Value, OrderFun, {RefVal, _S, _W, Smaller, Larger}, WRef) ->
                     end
             end;
         false ->
-            case tree_insert(Value, OrderFun, Larger, ?div2(WRef)) of
+            case tree_insert(Key, Val, Comp, Map, Larger, ?div2(WRef)) of
                 {ok, Idx, NewLarger} ->
                     NewIdx = tree_size(Smaller) + Idx + 1,
-                    {ok, NewIdx, combinate(RefVal, Smaller, NewLarger)};
+                    {ok, NewIdx, combinate(RefKey, Smaller, NewLarger)};
                 {check, Idx, NewLarger} ->
                     NewIdx = tree_size(Smaller) + Idx + 1,
-                    NewTree = combinate(RefVal, Smaller, NewLarger),
+                    NewTree = combinate(RefKey, Smaller, NewLarger),
                     {_, Size, Weight, _, _} = NewTree,
                     NewWRef = ?pow(Size),
                     if Weight > NewWRef -> {ok, NewIdx, balance_tree(NewTree)};
@@ -141,25 +176,25 @@ tree_insert(Value, OrderFun, {RefVal, _S, _W, Smaller, Larger}, WRef) ->
             end
     end;
 
-tree_insert(Value, _OrderFun, nil, 0) ->
-    {check, 1, combinate(Value, nil, nil)};
+tree_insert(Key, _Val, _Comp, _Map, nil, 0) ->
+    {check, 1, combinate(Key, nil, nil)};
 
-tree_insert(Value, _OrderFun, nil, _) ->
-    {ok, 1, combinate(Value, nil, nil)}.
+tree_insert(Key, _Val, _Comp, _Map, nil, _) ->
+    {ok, 1, combinate(Key, nil, nil)}.
 
 
-tree_delete(Value, _OrderFun, {Value, _Size, _Weight, Smaller, Larger}) ->
+tree_delete(RefKey, _Val, _Comp, _Map, {RefKey, _S, _W, Smaller, Larger}) ->
     {tree_size(Smaller) + 1, tree_merge(Smaller, Larger)};
 
-tree_delete(Value, OrderFun, {RefVal, _Size, _Weight, Smaller, Larger}) ->
-    case OrderFun(Value, RefVal) of
+tree_delete(Key, Val, Comp, Map, {RefKey, _Size, _Weight, Smaller, Larger}) ->
+    case Comp(Val, erod_maps:value(RefKey, Map)) of
         true ->
-            {Idx, NewSmaller} = tree_delete(Value, OrderFun, Smaller),
-            {Idx, combinate(RefVal, NewSmaller, Larger)};
+            {Idx, NewSmaller} = tree_delete(Key, Val, Comp, Map, Smaller),
+            {Idx, combinate(RefKey, NewSmaller, Larger)};
         false ->
-            {Idx, NewBigger} = tree_delete(Value, OrderFun, Larger),
+            {Idx, NewBigger} = tree_delete(Key, Val, Comp, Map, Larger),
             NewIdx = tree_size(Smaller) + Idx + 1,
-            {NewIdx, combinate(RefVal, Smaller, NewBigger)}
+            {NewIdx, combinate(RefKey, Smaller, NewBigger)}
     end.
 
 
@@ -168,73 +203,73 @@ tree_merge(Smaller, nil) -> Smaller;
 tree_merge(nil, Larger) -> Larger;
 
 tree_merge(Smaller, Larger) ->
-    {Value, NewLarger} = tree_take_smallest(Larger),
-    combinate(Value, Smaller, NewLarger).
+    {Key, NewLarger} = tree_take_smallest(Larger),
+    combinate(Key, Smaller, NewLarger).
 
 
-tree_smallest({Value, _Size, _Weight, nil, _Larger}) ->
-    Value;
+tree_smallest({Key, _Size, _Weight, nil, _Larger}) ->
+    Key;
 
-tree_smallest({_Value, _Size, _Weight, Smaller, _Larger}) ->
+tree_smallest({_Key, _Size, _Weight, Smaller, _Larger}) ->
     tree_smallest(Smaller).
 
 
-tree_take_smallest({Value, _Size, _Weight, nil, Larger}) ->
-    {Value, Larger};
+tree_take_smallest({Key, _Size, _Weight, nil, Larger}) ->
+    {Key, Larger};
 
-tree_take_smallest({Value, _Size, _Weight, Smaller, Larger}) ->
+tree_take_smallest({Key, _Size, _Weight, Smaller, Larger}) ->
     {SmallestValue, NewSmaller} = tree_take_smallest(Smaller),
-    {SmallestValue, combinate(Value, NewSmaller, Larger)}.
+    {SmallestValue, combinate(Key, NewSmaller, Larger)}.
 
 
-tree_largest({Value, _Size, _Weight, _Smaller, nil}) ->
-    Value;
+tree_largest({Key, _Size, _Weight, _Smaller, nil}) ->
+    Key;
 
-tree_largest({_Value, _Size, _Weight, _Smaller, Larger}) ->
+tree_largest({_Key, _Size, _Weight, _Smaller, Larger}) ->
     tree_largest(Larger).
 
 
-tree_take_largest({Value, _Size, _Weight, Smaller, nil}) ->
-    {Value, Smaller};
+tree_take_largest({Key, _Size, _Weight, Smaller, nil}) ->
+    {Key, Smaller};
 
-tree_take_largest({Value, _Size, _Weight, Smaller, Larger}) ->
+tree_take_largest({Key, _Size, _Weight, Smaller, Larger}) ->
     {LargestValue, NewLarger} = tree_take_largest(Larger),
-    {LargestValue, combinate(Value, Smaller, NewLarger)}.
+    {LargestValue, combinate(Key, Smaller, NewLarger)}.
 
 
-balance_tree({_Value, Size, _Weight, _Smaller, _Larger} = Tree) ->
+balance_tree({_Key, Size, _Weight, _Smaller, _Larger} = Tree) ->
     balance_list(tree_to_list(Tree), Size).
 
 
-balance_list(Values, Size) ->
-    {Tree, []} = balance_branch(Values, Size),
+balance_list(Keys, Size) ->
+    {Tree, []} = balance_branch(Keys, Size),
     Tree.
 
 
-balance_branch(Values, Size) when Size > 1 ->
+balance_branch(Keys, Size) when Size > 1 ->
     BothSize = Size - 1,
     LargerSize = BothSize div 2,
     SmallerSize = BothSize - LargerSize,
-    {Smaller, [Pivot | Remaining1]} = balance_branch(Values, SmallerSize),
+    {Smaller, [Pivot | Remaining1]} = balance_branch(Keys, SmallerSize),
     {Larger, Remaining2} = balance_branch(Remaining1, LargerSize),
     {combinate(Pivot, Smaller, Larger), Remaining2};
 
-balance_branch([Value |Values], 1) ->
-    {combinate(Value, nil, nil), Values};
+balance_branch([Key |Keys], 1) ->
+    {combinate(Key, nil, nil), Keys};
 
-balance_branch(Values, 0) ->
-    {nil, Values}.
+balance_branch(Keys, 0) ->
+    {nil, Keys}.
 
 
-combinate(V, {_V1, S1, H1, _Sm1, _Lg1} = Sm, {_V2, S2, H2, _Sm2, _Lg2} = Lg) ->
-    {V, S1 + S2 + 1, ?mul2(erlang:max(H1, H2)), Sm, Lg};
+combinate(K, {_K1, S1, H1, _Sm1, _Lg1} = Sm, {_K2, S2, H2, _Sm2, _Lg2} = Lg) ->
+    {K, S1 + S2 + 1, ?mul2(erlang:max(H1, H2)), Sm, Lg};
 
-combinate(V, {_V1, S1, H1, _Sm1, _Lg1} = Sm, nil) ->
-    {V, S1 + 1, ?mul2(H1), Sm, nil};
+combinate(K, {_K1, S1, H1, _Sm1, _Lg1} = Sm, nil) ->
+    {K, S1 + 1, ?mul2(H1), Sm, nil};
 
-combinate(V, nil, {_V2, S2, H2, _Sm2, _Lg2} = Lg) ->
-    {V, S2 + 1, ?mul2(H2), nil, Lg};
+combinate(K, nil, {_K2, S2, H2, _Sm2, _Lg2} = Lg) ->
+    {K, S2 + 1, ?mul2(H2), nil, Lg};
 
-combinate(V, nil , nil) ->
-    {V, 1, 1, nil, nil}.
+combinate(K, nil , nil) ->
+    {K, 1, 1, nil, nil}.
 
